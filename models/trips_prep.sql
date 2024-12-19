675546e4c7a2649a1ea0fa2e23df30002d489efc
Vlad <selytin99@gmail.com>
2024-12-19 20:40:57 +0300

    Создал новую модель с причесанными поездками

---
+++ models/trips_prep.sql
@@ -0,0 +1,16 @@
+select
+	id, 
+	user_id, 
+	scooter_hw_id, 
+	started_at, 
+	finished_at, 
+	extract(epoch from (finished_at - started_at)) as duration_s,
+	start_lat, 
+	start_lon, 
+	finish_lat, 
+	finish_lon, 
+	distance as distance_m, 
+	cast(price as decimal(20, 2)) / 100 as price_rub,
+	cast(case when price = 0 and finished_at > started_at then 1 else 0 end as boolean) as is_free,
+	date(started_at) as date
+from {{ source("scooters_raw","trips") }}
