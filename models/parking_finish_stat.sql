c877a620dfd3f2ccd423b1a65ebea7d2b4ee1a3b
Vlad <selytin99@gmail.com>
2024-12-20 11:59:39 +0300

    Добавил модели с геометрическими вычислениями точек старта и финиша

---
+++ models/parking_finish_stat.sql
@@ -0,0 +1,9 @@
+select
+    st_transform(hex.geom, 4326) as geom,
+    count(*) as trips
+from {{ ref("trips_geom")}} as t
+cross join st_hexagongrid(10, st_transform(t.finish_point, 3857)) as hex
+where 1=1
+    and st_intersects(st_transform(t.finish_point, 3857), hex.geom)
+group by
+    st_transform(hex.geom, 4326)
