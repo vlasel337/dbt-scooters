fb91d50fa5be6b6fc1c1a1842e53d21ce211dad7
Vlad <selytin99@gmail.com>
2024-12-23 20:10:59 +0300

    6_3 Добавил инкрементальную модель с выручкой по дням (стратегия merge)

---
+++ models/revenue_daily.sql
@@ -0,0 +1,15 @@
+select 
+    "date", 
+    sum(price_rub) as revenue_rub,
+    now() at time zone 'utc' as updated_at
+from {{ ref("trips_prep") }}
+
+{% if is_incremental() %}
+where 1=1
+    and "date" >= (select date(max("date")) - INTERVAL '2 days' from {{ this }})
+
+{% endif %}
+group by 
+    "date", 
+    now() at time zone 'utc'
+order by "date"
